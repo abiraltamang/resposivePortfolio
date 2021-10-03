69c3e9109fdd2f94c25c762abb50171ef02b23ae
abiraltamang <abiral.tamang816@gmail.com>
2021-10-03 19:49:32 +0545

    Hamburger icon created

--- js/app.js
+++ js/app.js
@@ -5,5 +5,21 @@ $('.slider').slick({
     appendDots:'.slider-dots',
     dotsClass:'dots',
 });
-    
+  
+
+let hamburger=document.querySelector('.hamburger');
+let times= document.querySelector('.times');
+let mobileNav= document.querySelector('.mobile-nav');
+
+
+hamburger.addEventListener('click',function () {
+    mobileNav.classList.add('open');
 });
+
+times.addEventListener('click',function () {
+    mobileNav.classList.remove('open');
+});
+
+});
+
+
